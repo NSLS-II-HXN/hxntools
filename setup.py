8a7d2ed17510d25a5ca0cadbd93d1312dcb564ac
Maksim Rakitin <mrakitin@bnl.gov>
2020-09-03 11:20:07 -0400

    Add infrastructure files

--- setup.py
+++ setup.py
@@ -1,24 +1,39 @@
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
 
-import sys
-import warnings
+import os
+# To use a consistent encoding
+from codecs import open
 
+import setuptools
 
-try:
-    from setuptools import setup
-except ImportError:
-    try:
-        from setuptools.core import setup
-    except ImportError:
-        from distutils.core import setup
+import versioneer
 
-from distutils.core import setup
+here = os.path.abspath(os.path.dirname(__file__))
 
-setup(
-    name='hxntools',
-    version="0.4.0",
-    author='Brookhaven National Laboratory',
-    packages=['hxntools', 'hxntools.detectors', 'hxntools.handlers'],
-    install_requires=['numpy', 'h5py', 'databroker', 'nslsii'],
+description = "NSLS-II Hard X-ray Nanoprobe data acquisition tools"
 
+# Get the long description from the README file
+readme_file = os.path.join(here, 'README.md')
+long_description = description
+if os.path.isfile(readme_file):
+    with open(readme_file, encoding='utf-8') as f:
+        long_description = f.read()
+
+req_file = os.path.join(here, 'requirements.txt')
+requirements = []
+if os.path.isfile(req_file):
+    with open(req_file) as f:
+        requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="hxntools",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="BSD-3-Clause",
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description=description,
+    author="Brookhaven National Laboratory",
+    install_requires=requirements,
 )
