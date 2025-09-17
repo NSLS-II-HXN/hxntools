223cd97bf201ddfceeba7fa9bebd123219fd864c
HXN Operator <xf03id@xf03idc-ws8.nsls2.bnl.local>
2025-09-17 17:51:40 -0400

    Recalibrated the ppmac to reflect the correct zpss* stages position; hxntools applies the conversion ratio from readback voltage to the position

--- hxntools/motor_info.py
+++ hxntools/motor_info.py
@@ -1,5 +1,5 @@
 motor_table = {'zpssx':('inenc2_val',-1e-4*0.9542,'/INENC2.VAL.Value'), #-9.7e-5),
-            'zpssy':('inenc3_val',-1e-4,'/INENC3.VAL.Value'), #-1.006e-4),
+            'zpssy':('inenc3_val',-1.03297e-4,'/INENC3.VAL.Value'), #-1.006e-4),
             'zpssz':('inenc4_val',1e-4,'/INENC4.VAL.Value'), #1.04e-4),
             'dssx':('inenc2_val',-1e-4,'/INENC2.VAL.Value'),
             'dssy':('inenc3_val',1e-4,'/INENC3.VAL.Value'),
